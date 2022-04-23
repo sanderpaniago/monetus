83b5190ff1f01869dde23da2f5070e1f70516255
Sander Paniago <sanderppaniago@gmail.com>
2022-04-23 15:39:44 -0400

    :bug: fix erro url production

--- config.js
+++ config.js
@@ -1,14 +1,20 @@
 module.exports = {
   lighthouse: {
-    server: process.env.NEXT_PUBLIC_VERCEL_URL
-      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-      : 'http://localhost:3000',
+    server:
+      process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+        ? 'https://monetus.sanderpaniago.dev'
+        : process.env.NEXT_PUBLIC_VERCEL_ENV
+        ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+        : 'http://localhost:3000',
     pages: {
       home: '/'
     }
   },
 
-  baseUrl: process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-    : 'http://localhost:3000'
+  baseUrl:
+    process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+      ? 'https://monetus.sanderpaniago.dev'
+      : process.env.NEXT_PUBLIC_VERCEL_ENV
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+      : 'http://localhost:3000'
 }
