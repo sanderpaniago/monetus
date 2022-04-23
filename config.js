d2e30a6e4a683882acb637bb21fdb5c87d2ebd8b
Sander Paniago <sanderppaniago@gmail.com>
2022-04-23 03:55:05 -0400

    testing environment

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   lighthouse: {
-    server: process.env.VERCEL_URL || 'http://localhost:3000',
+    server: process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000',
     pages: {
       home: '/'
     }
   },
 
-  baseUrl: process.env.VERCEL_URL || 'http://localhost:3000'
+  baseUrl: process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000'
 }
